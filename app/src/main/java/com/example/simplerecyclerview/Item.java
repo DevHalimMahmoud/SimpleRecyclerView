d949fe4e5de8f1e48fa45ab49e33e78e0ae52a27
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2020-09-05 22:45:31 +0200

    .

---
+++ app/src/main/java/com/example/simplerecyclerview/Item.java
@@ -0,0 +1,40 @@
+package com.example.simplerecyclerview;
+
+
+public class Item {
+
+    private String name;
+    private String title;
+    private String id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public Item(String name, String title, String id) {
+        this.name = name;
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
